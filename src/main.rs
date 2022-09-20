7ba91ac6cda0a86261200f3af01a5f4cfa99befa
Fred Campos <mail2frede@gmail.com>
2022-09-20 01:10:51 -0300

    add: impl, traits and macros

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use utils::{Person, File, GetAdditionalInfo};
 use utils::check_type;
 use utils::mars::{read_weight_on_earth, calculate_weight_on_mars};
 
 mod utils;
 
 fn main() {
+
+    let person = Person::new(String::from("Fred"), 35, String::from("Bololo"));
+    let file = File::new(String::from(r#"C:\file.txt"#), 1024, String::from("Empty text file"));
+
+    println!("Person name is: {} and age is: {}", person.get_name(), person.get_age());
+    println!("Person additional info is: {}", person.get_additional_info());
+    println!("Person additional info type: {}", person.get_additional_info_type());
+
+    println!("File path is: {}", file.get_path());
+    println!("File size is: {}", file.get_size());
+    println!("File additional infor is: {}", file.get_additional_info());
+
     println!("Enter weight at earth: ");
     let weight_on_earth :f32 = read_weight_on_earth();
     let weight_on_mars :f32 = calculate_weight_on_mars(weight_on_earth);
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,75 @@
 use std::io;
 
+pub struct Person<T> {
+   name: String,
+   age: u8,
+   additional_info: T,
+}
+
+pub struct File<T> {
+    path: String,
+    size: u32,
+    additional_info: T,
+}
+
+pub trait GetAdditionalInfo<T> {
+    fn get_additional_info(&self) -> &T;
+    fn get_additional_info_type(&self) -> &'static str;
+}
+
+#[macro_export]
+macro_rules! get_additional_info_impl_for_struct {
+    ($struct_name: ident) => {
+        impl <T> GetAdditionalInfo<T> for $struct_name<T> {
+            fn get_additional_info(&self) -> &T {
+                &self.additional_info
+            }
+
+            fn get_additional_info_type(&self) -> &'static str {
+                check_type(&self.additional_info)
+            }
+
+        }
+    };
+}
+
+impl<T> Person<T> {
+    pub fn new(name: String, age: u8, additional_info: T) -> Self {
+        get_additional_info_impl_for_struct!(Person);
+        Person { name, age, additional_info }
+    }
+
+    pub fn get_age(&self) -> &u8 {
+        &self.age
+    }
+
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+}
+
+impl<T> File<T> {
+    pub fn new(path: String, size: u32, additional_info: T) -> Self {
+        get_additional_info_impl_for_struct!(File);
+        File { path, size, additional_info }
+    }
+
+    pub fn get_path(&self) -> &String {
+        &self.path
+    }
+
+    pub fn get_size(&self) -> &u32 {
+        &self.size
+    }
+}
+
 pub fn check_type<T>(_: T) -> &'static str {
     std::any::type_name::<T>()
 }
+
+// pub fn instanceof<T>(type1: T, type2: T) -> bool {
+//     check_type(type1) == check_type(type2)
+// }
 
 pub mod mars {
 
